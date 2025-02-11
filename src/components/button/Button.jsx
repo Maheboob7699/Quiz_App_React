b996b361dc980fa2c7e4fb84317ed4b0639ff7d2
Maheboob7699 <murdimaheboob@gmail.com>
2025-02-11 08:39:09 -0900

    create a next button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,11 @@
 import '../button/button.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 function Button({title,textName}){
     return(
         <>
-          <button className={textName}>{title}</button>
+          <button className={textName}>{title} {title === "Next" && <FontAwesomeIcon icon={faArrowRight} />}
+          </button>
         </>
     )
 }
