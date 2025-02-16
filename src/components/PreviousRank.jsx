a0c5c1f1f9556614901d0455a2f1b03bc2ab5691
Maheboob7699 <murdimaheboob.com>
2025-02-16 15:57:29 +0530

    changes in h1 tag

--- src/components/PreviousRank.jsx
+++ src/components/PreviousRank.jsx
@@ -1,8 +1,9 @@
 import '../assets/styles/PreviousRank.css';
-function PreviousRank(){
+function PreviousRank({user}){
     return(
         <>
          <div className='lower-rank'>
+            {/* <h2>{user ? user.name:"user not found"}</h2> */}
          </div>
         </>
     )
